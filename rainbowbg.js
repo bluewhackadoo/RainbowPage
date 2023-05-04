24b11503adcc8e68975257e7c2eca56fa47ac55d
Eric Kratzer <erickr@microsoft.com>
2023-05-04 12:58:56 -0700

    move to shadertoy

--- rainbowbg.js
+++ rainbowbg.js
@@ -1,20 +1,20 @@
 var mo = false;
 
 function changeBackground(color) {
-document.body.style.background = color;
+    document.body.style.background = color;
 }
 
-window.addEventListener("load",function() { changeBackground('Black') });
+window.addEventListener("load", function () { changeBackground('Black') });
 
 // works out the X, Y position of the click inside the canvas from the X, Y position on the page
 function getPosition(mouseEvent, sigCanvas) {
     var x, y;
     if (mouseEvent.pageX != undefined && mouseEvent.pageY != undefined) {
-    x = mouseEvent.pageX;
-    y = mouseEvent.pageY;
+        x = mouseEvent.pageX;
+        y = mouseEvent.pageY;
     } else {
-    x = mouseEvent.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    y = mouseEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        x = mouseEvent.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = mouseEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
     }
 
     return { X: x - sigCanvas.offsetLeft, Y: y - sigCanvas.offsetTop };
@@ -31,7 +31,7 @@ function initialize() {
     //context.canvas.height = window.innerHeight;
     window.addEventListener('resize', resizeCanvas, false);
     resizeCanvas(sigCanvas);
-
+    drawBackg(context);
 
     // start drawing when the mousedown event fires, and attach handlers to
     // draw a line to wherever the mouse moves to
@@ -40,19 +40,17 @@ function initialize() {
 
         // attach event handlers
         $(this).mousemove(function (mouseEvent) {
-            
-            if(mo == true)
-            {    
+
+            if (mo == true) {
                 mo = false;
             }
-            else
-            {
+            else {
                 drawRect(mouseEvent, sigCanvas, context);
             }
         }).mouseout(function (mouseEvent) {
             mo = true;
 
-                drawBackg(context);
+            drawBackg(context);
 
             //stroke(mouseEvent, sigCanvas, context);
             //finishDrawing(mouseEvent, sigCanvas, context);
@@ -68,6 +66,7 @@ function initialize() {
 
 function drawBackg(context) {
     context.rect(0, 0, window.innerWidth, window.innerHeight);
+
     var grad = context.createLinearGradient(0, window.innerHeight, 0, 0)
     grad.addColorStop(0.01, 'Red');
     grad.addColorStop(0.15, 'Orange');
@@ -79,55 +78,53 @@ function drawBackg(context) {
     //grad.addColorStop(.005, 'gray');
     //grad.addColorStop(.9, 'green');
     context.fillStyle = grad;
+    //draw this in the background
+
     context.fill();
 }
 
 function drawRect(mouseEvent, sigCanvas, context) {
-    var position = getPosition(mouseEvent, sigCanvas); 
-    if(mo == true)
-    {
-    mo = false;
-    rec.stroke();
+    var position = getPosition(mouseEvent, sigCanvas);
+    if (mo == true) {
+        mo = false;
+        rec.stroke();
     }
-    else
-    {
-    drawBackg(context);
+    else {
+        drawBackg(context);
 
-    context.rect(position.X - 50, position.Y - 50, 100 , 100);
-    var grad = context.createRadialGradient(position.X, position.Y, 0, position.X, position.Y, 50)
-    //grad.addColorStop(0.1, 'darkgrey');
-    //grad.addColorStop(1, 'black');
-    grad.addColorStop(0, 'Red');
-    grad.addColorStop(0.15, 'Orange');
-    grad.addColorStop(0.3, 'yellow');
-    grad.addColorStop(0.45, 'Green');
-    grad.addColorStop(0.6, 'Blue');
-    grad.addColorStop(0.75, 'Indigo');
-    grad.addColorStop(0.9, 'Violet');
-    grad.addColorStop(0.91, 'Transparent');
-    context.fillStyle = grad;
-    context.fill();
-    //context.fill();
-    //context.lineTo(position.X, position.Y);
-    //context.stroke();
+        context.rect(position.X - 50, position.Y - 50, 100, 100);
+        var grad = context.createRadialGradient(position.X, position.Y, 0, position.X, position.Y, 50)
+        //grad.addColorStop(0.1, 'darkgrey');
+        //grad.addColorStop(1, 'black');
+        grad.addColorStop(0, 'Red');
+        grad.addColorStop(0.15, 'Orange');
+        grad.addColorStop(0.3, 'yellow');
+        grad.addColorStop(0.45, 'Green');
+        grad.addColorStop(0.6, 'Blue');
+        grad.addColorStop(0.75, 'Indigo');
+        grad.addColorStop(0.9, 'Violet');
+        grad.addColorStop(0.91, 'Transparent');
+        context.fillStyle = grad;
+        context.fill();
+        //context.fill();
+        //context.lineTo(position.X, position.Y);
+        //context.stroke();
     }
 }
 
 // draws a line to the x and y coordinates of the mouse event inside
 // the specified element using the specified context
 function drawLine(mouseEvent, sigCanvas, context) {
-    var position = getPosition(mouseEvent, sigCanvas); 
-    if(mo == true)
-    {
-    mo = false;
-    context.stroke();
+    var position = getPosition(mouseEvent, sigCanvas);
+    if (mo == true) {
+        mo = false;
+        context.stroke();
     }
-    else
-    {
+    else {
 
-    context.lineTo(position.X, position.Y);
-    //context.arcTo(position.X, position.Y);
-    context.stroke();
+        context.lineTo(position.X, position.Y);
+        //context.arcTo(position.X, position.Y);
+        context.stroke();
     }
 }
 
@@ -142,13 +139,13 @@ function finishDrawing(mouseEvent, sigCanvas, context) {
 
     // unbind any events which could draw
     $(sigCanvas).unbind("mousemove")
-                .unbind("mouseup")
-                .unbind("mouseout");
+        .unbind("mouseup")
+        .unbind("mouseout");
 }
 
 function resizeCanvas(canvas) {
-    
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    
+
 }
